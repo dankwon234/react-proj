240a4257377363d0daf36a5a41c2954d49e602fc
dankwon234 <dan.kwon234@gmail.com>
2016-03-18 13:05:04 -0400

    lists

--- public/app/App.js
+++ public/app/App.js
@@ -1,14 +1,98 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var List = require('./components/List');
+var Header = require('./components/Header');
 
 var App = React.createClass({
+	getInitialState: function(){
+		return {
+			nextMember: {
+				id: null,
+				name: '',
+				city: '',
+				gender: '',
+				type:'vip'
+			},
+			members: [
+				{id:1, name:'donald trump', city:'new york', gender:'m'},
+				{id:2, name:'hillary clinton', city:'new york', gender:'f'},
+				{id:3, name:'bernie sanders', city:'burlington', gender:'m'}
+			],
+			vipMembers:[
+				{id:1, name:'rihanna', city:'new york', gender:'f'},
+				{id:2, name:'leonardo dicaprio', city:'los angeles', gender:'m'},
+				{id:3, name:'justin bieber', city:'vancouver', gender:'m'}
+			]
+		}
+	},
+
+	addMember: function(){
+		var newMember = {
+			id: this.state.nextMember.id,
+			name: this.state.nextMember.name,
+			city: this.state.nextMember.city,
+			gender: this.state.nextMember.gender
+		}
+
+		console.log('Add Member: '+JSON.stringify(newMember));
+
+		if (this.state.nextMember.type == 'vip'){
+			var currentList = this.state.vipMembers;
+			currentList.push(newMember);
+			this.setState({
+				vipMembers: currentList,
+				nextMember: {
+					id: null,
+					name:'',
+					city:'',
+					gender:'',
+					type:'vip'
+				}
+			});
+		}
+		else{
+			var currentList = this.state.members;
+			currentList.push(newMember);
+			this.setState({
+				members: currentList,
+				nextMember: {
+					id: null,
+					name:'',
+					city:'',
+					gender:'',
+					type:'vip'
+				}
+			});
+		}
+
+	},
+
+	nextMemberUpdated: function(event){
+		console.log(event.target.id+': '+event.target.value);
+		this.state.nextMember[event.target.id] = event.target.value;
+		this.setState({
+			nextMember: this.state.nextMember
+		});
+	},
 
 	render: function(){
 		return (
 			<div>
-				Show me the REACT!
-				<List />
+				<Header />
+				<List type="VIP" roster={this.state.vipMembers} />
+				<List type="Election" roster={this.state.members} />
+
+				<hr />
+				<h2>Next Member</h2>
+				<input value={this.state.nextMember.name} onChange={this.nextMemberUpdated} type="text" name="name" id="name" placeholder="Name" /><br />
+				<input value={this.state.nextMember.city} onChange={this.nextMemberUpdated} type="text" name="city" id="city" placeholder="City" /><br />
+				<input value={this.state.nextMember.gender} onChange={this.nextMemberUpdated} type="text" name="gender" id="gender" placeholder="Gender" /><br />
+				<input value={this.state.nextMember.id} onChange={this.nextMemberUpdated} type="text" name="id" id="id" placeholder="ID Number" /><br />
+				<input value={this.state.nextMember.type} onChange={this.nextMemberUpdated} type="text" name="type" id="type" placeholder="Type" /><br />
+				<br />
+				<button onClick={this.addMember}>Add Member</button>
+
+
 			</div>
 		);
 	}
---
+++ public/app/components/Header.js
@@ -0,0 +1,22 @@
+var React = require('react');
+
+var Header = React.createClass({
+
+	getInitialState: function(){
+		return {
+
+		}
+	},
+
+	render: function(){
+
+		return (
+			<div>
+				<h1>Welcome To React!</h1>
+
+			</div>
+		);
+	}
+});
+
+module.exports = Header;
